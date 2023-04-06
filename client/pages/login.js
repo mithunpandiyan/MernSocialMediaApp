b17b4193d70941369dc158923cd1bb5c8f25e2a7
mithun <mithun.p@codeboardtech.com>
2023-04-06 16:33:13 +0530

    first push

---
+++ client/pages/login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Login Page</h1>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
